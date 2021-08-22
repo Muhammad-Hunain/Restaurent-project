a494642c8dd2b1bf9b8d78e69f4ffb05190146c2
Muhammad-Hunain <hunain29.mem@gmail.com>
2021-08-22 21:00:00 +0500

    Project uploaded

---
+++ media/js/firebase.js
@@ -0,0 +1,12 @@
+
+  
+  var firebaseConfig = {
+    apiKey: "AIzaSyCCmTP8JLUGYpa8reDsunkpsBmPW8OvBdI",
+    authDomain: "restaurent-710da.firebaseapp.com",
+    projectId: "restaurent-710da",
+    storageBucket: "restaurent-710da.appspot.com",
+    messagingSenderId: "566116390603",
+    appId: "1:566116390603:web:6039ca0ff2a9ccce73ad4e"
+  };
+ 
+  firebase.initializeApp(firebaseConfig);
